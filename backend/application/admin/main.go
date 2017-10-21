59259fa07288c5c0bf1e2b28bf40b9d11db87227
yangchenglong11 <1332456142@qq.com>
2017-10-21 12:32:44 +0800

    修改目录结构
    将  admin 和 user 分别放在不同目录

---
+++ backend/application/admin/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "github.com/Jsharkc/TechTree/backend/routers/user"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ backend/controllers/base.go
@@ -0,0 +1,10 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type BaseController struct {
+	beego.Controller
+}
+
