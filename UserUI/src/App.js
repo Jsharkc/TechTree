916695c76bf75f6a631e2bc564765c695e18890d
wangriyu <wangriyu1997@gmail.com>
2017-10-21 12:50:29 +0800

    create react app for UserUI

---
+++ UserUI/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <br />
+        <Button>
+          add
+        </Button>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App;
