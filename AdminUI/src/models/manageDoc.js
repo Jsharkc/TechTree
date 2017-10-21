7bc029e9e2c690bff5f90e96aeb7e3bfc346b252
wangriyu <wangriyu1997@gmail.com>
2017-10-22 06:38:52 +0800

    添加表项 #27

--- AdminUI/src/models/manageDoc.js
+++ AdminUI/src/models/manageDoc.js
@@ -7,12 +7,101 @@ export default {
   namespace: 'manageDoc',
 
   state: {
+    previewVisible: false,
+    previewNode: {},
+    dataSource: [{
+      id: 1,
+      name: '加速度',
+      submit: {
+        node: 'go',
+        content: '暗示明年打算的'
+      }
+    }, {
+      id: 2,
+      name: '是的烦恼',
+      submit: {
+        node: 'ksjd',
+        content: '网文件筐违反络客服没玩了法'
+      }
+    }]
+  },
+
+  subscriptions: {
+    setup ({ dispatch, history }) {
+      history.listen(location => {
+        if (location.pathname === '/manageDoc/submitDoc') {
+          dispatch({
+            type: 'query',
+            payload: location.query,
+          })
+        }
+      })
+    }
   },
 
   effects: {
+    * query ({ payload }, { call, put }) {
+      // const res = yield call(queryTableOrder, {})
+      //
+      // if (res.status === 0) {
+      //   yield put({
+      //     type: 'querySuccess',
+      //     payload: {
+      //       list: res.resp,
+      //     }
+      //   })
+      // } else {
+      //   message.error(Strings.Common.QueryFailed)
+      // }
+    },
 
+    * confirmTable ({ payload }, { call, put }) {
+      // const res = yield call(confirmTableOrder, { id: payload })
+      //
+      // if (res.status === 0) {
+      //   yield put({
+      //     type: 'query'
+      //   })
+      // } else {
+      //   message.error(Strings.Common.FailedOption)
+      // }
+    },
+
+    * releaseTable ({ payload }, { call, put }) {
+      // const res = yield call(releaseTableOrder, { id: payload })
+      //
+      // if (res.status === 0) {
+      //   yield put({
+      //     type: 'query'
+      //   })
+      // } else {
+      //   message.error(Strings.Common.FailedOption)
+      // }
+    }
   },
 
   reducers: {
+    querySuccess (state, action) {
+      const { dataSource } = action.payload
+      return {
+        ...state,
+        dataSource
+      }
+    },
+
+    showPreview (state, action) {
+      return {
+        ...state,
+        previewNode: action.payload,
+        previewVisible: true
+      }
+    },
+
+    hidePreview (state) {
+      return {
+        ...state,
+        previewVisible: false
+      }
+    },
   }
 }
--- AdminUI/src/routes/manageDoc/submitDoc/index.js
+++ AdminUI/src/routes/manageDoc/submitDoc/index.js
@@ -3,15 +3,65 @@
  *     Initial: 2017/10/22        Wang RiYu
  */
 
-import React    from 'react';
-import { Card } from 'antd';
+import React       from 'react'
+import { connect } from 'dva'
+import DocList     from './docList'
+import {
+  Card,
+  Modal,
+}                  from 'antd'
 
-const SubmitDoc = () => {
+const SubmitDoc = ({ manageDoc, dispatch, loading }) => {
+  const {
+    dataSource,
+    previewVisible,
+    previewNode
+  } = manageDoc
+
+  const DocListProps = {
+    dataSource,
+    loading,
+    confirm (id) {
+      dispatch({
+        type: 'manageDoc/confirmTable',
+        payload: id
+      })
+    },
+    release (id) {
+      dispatch({
+        type: 'manageDoc/releaseTable',
+        payload: id
+      })
+    },
+    onPreview (tNode) {
+      dispatch({
+        type: 'manageDoc/showPreview',
+        payload: tNode
+      })
+    }
+  }
+
+  const PreviewModalProps = {
+    title: <div style={{fontSize: '16px'}}>知识点{previewNode.node}</div>,
+    visible: previewVisible,
+    footer: null,
+    onCancel () {
+      dispatch({
+        type: 'manageDoc/hidePreview'
+      })
+    }
+  }
+
   return (
-    <Card>
-      submit doc
+    <Card style={{minHeight: '600px'}}>
+      <DocList {...DocListProps} />
+      <Modal {...PreviewModalProps}>
+        <div>{previewNode.content || ''}</div>
+      </Modal>
     </Card>
   )
 }
 
-export default SubmitDoc
+export default connect(
+  ({ manageDoc, loading }) => ({ manageDoc, loading: loading.models.manageDoc })
+)(SubmitDoc)
