0a6c6200f7eaa4a743745cdc724926496d01621c
wangriyu <wangriyu1997@gmail.com>
2017-10-21 13:47:42 +0800

    add dva model

--- UserUI/src/index.js
+++ UserUI/src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import dva from 'dva';
+import { browserHistory } from 'dva/router';
+import createLoading from 'dva-loading';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// 1. Initialize
+const app = dva({
+  ...createLoading(),
+  history: browserHistory,
+});
+
+// 2. Plugins
+// app.use({});
+
+// 3. Model
+app.model(require('./models/example'));
+
+// 4. Router
+app.router(require('./router'));
+
+// 5. Start
+app.start('#root');
